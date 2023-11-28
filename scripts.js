32ba2c60c2c7a96d8eee6ed7ea9d6cf1874ef1c4
Ramon Cisneros <cisneroslopezramon1@gmail.com>
2023-11-28 09:19:37 -0600

    Se agregaron funciones para calcular la suma total y se agregó un array para automatizar

--- scripts.js
+++ scripts.js
@@ -8,19 +8,53 @@ function extraerNumeroDesdeElemento(elemento) {
 
 // Hacemos una funcion para mostrar total de ventas en las tiendas
 function calcular(){
-    let ventas1, ventas2, ventas3, ventas4, ventas5, ventas6;
+    let ventas = [];
     
-    ventas1 = extraerNumeroDesdeElemento("ventasTienda1");
-    ventas2 = extraerNumeroDesdeElemento("ventasTienda2");
-    ventas3 = extraerNumeroDesdeElemento("ventasTienda3");
-    ventas4 = extraerNumeroDesdeElemento("ventasTienda4");
-    ventas5 = extraerNumeroDesdeElemento("ventasTienda5");
-    ventas6 = extraerNumeroDesdeElemento("ventasTienda6");
+    ventas[0] = extraerNumeroDesdeElemento("ventasTienda1");
+    ventas[1] = extraerNumeroDesdeElemento("ventasTienda2");
+    ventas[2] = extraerNumeroDesdeElemento("ventasTienda3");
+    ventas[3] = extraerNumeroDesdeElemento("ventasTienda4");
+    ventas[4] = extraerNumeroDesdeElemento("ventasTienda5");
+    ventas[5] = extraerNumeroDesdeElemento("ventasTienda6");
 
-    let totalVentas = ventas1 + ventas2 + ventas3 + ventas4 + ventas5 + ventas6;
+    let totalVentas = sumarTotal(ventas);
+    let ventaMayorLlamada = ventaMayor(ventas);
+    let ventaMenorLlamada = ventaMenor(ventas);
+    
 
-    let mensajeVentas = `Total Ventas: ${totalVentas}`;
+    let mensajeVentas = `Total Ventas: ${totalVentas} <br> Venta Mayor: ${ventaMayorLlamada} <br> Venta Menor: ${ventaMenorLlamada}`;
     let parrafoMostrarVentas = document.getElementById("parrafoMostrarVentas");
 
-    parrafoMostrarVentas.textContent = mensajeVentas;
+    parrafoMostrarVentas.innerHTML = `${mensajeVentas}`;
+}
+
+function sumarTotal(array) {
+    let total = 0;
+
+    for(let venta of array) {
+        total += venta;
+    }
+    return total; 
+}
+
+function ventaMayor(array) {
+    let mayor = array[0]
+
+    for(let venta = 0; venta < array.length; venta++){
+        if (array[venta] > mayor) {
+            mayor = array[venta];
+        }
+    }
+    return mayor;
+}
+
+function ventaMenor(array) {
+    let menor = array[0]
+
+    for(let venta = 0; venta < array.length; venta++){
+        if (array[venta] < menor) {
+            menor = array[venta];
+        }
+    }
+    return menor;
 }
